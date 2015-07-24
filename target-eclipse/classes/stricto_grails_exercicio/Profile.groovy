a2775b2d05196f9015b387c61bd8c5b405213b74
fabricio <fabricio@debian>
2015-07-24 10:37:26 -0300

    atualizacao

---
+++ target-eclipse/classes/stricto_grails_exercicio/Profile.groovy
@@ -0,0 +1,26 @@
+package stricto_grails_exercicio
+
+class Profile {
+	
+	byte[] photo
+	String fullName
+	String bio
+	String homepage
+	String email
+	String timezone
+	String country
+	String jabberAddress
+	
+	static constraints = {
+		fullName(nullable: true)
+		bio(nullable: true, maxSize: 1000)
+		homepage(url: true, nullable: true)
+		email(email: true, nullable: true)
+		photo(nullable: true, maxSize: 51200)
+		country(nullable: true)
+		timezone(nullable: true)
+		jabberAddress(email: true, nullable: true)
+	}
+
+	static belongsTo = User
+}
